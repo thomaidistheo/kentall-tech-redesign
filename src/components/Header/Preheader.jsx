66e24358c22d5b7498211929dce26416de9bbb27
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2023-03-09 15:43:34 +0200

    changes in max-width for hero img and change in preheader max-width

--- src/components/Header/Preheader.jsx
+++ src/components/Header/Preheader.jsx
@@ -8,24 +8,26 @@ import { ReactComponent as IconTel } from '../../assets/icons/phone-icon.svg'
 export default function Preheader() {
   return (
     <div className="preheader">
-      <ul>
-        <li>
-          <a href="tel:2170001200">
-            <span className='preheader-icon'><IconTel /></span>
-            <span className="link-text preheader-text">2170001200</span>
-          </a>
-          <span className='space'>-</span>
-          <a className="second-phoneno" href="tel:2107663471">
-            <span className="link-text preheader-text">2107663471</span>
-          </a>
-        </li>
-        <li>
-          <a href="mailto:info@kentall-tech.com">
-            <span className='preheader-icon'><IconMail /></span>
-            <span className="link-text preheader-text">info@kentall-tech.com</span>
-          </a>
-        </li>
-      </ul>
+      <div className="preheader__content">
+        <ul>
+          <li>
+            <a href="tel:2170001200">
+              <span className='preheader-icon'><IconTel /></span>
+              <span className="link-text preheader-text">2170001200</span>
+            </a>
+            <span className='space'>-</span>
+            <a className="second-phoneno" href="tel:2107663471">
+              <span className="link-text preheader-text">2107663471</span>
+            </a>
+          </li>
+          <li>
+            <a href="mailto:info@kentall-tech.com">
+              <span className='preheader-icon'><IconMail /></span>
+              <span className="link-text preheader-text">info@kentall-tech.com</span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
